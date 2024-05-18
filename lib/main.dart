e16c29cc2ef54dcdf7ce69b59db609d5d669c9d4
Yash Rajput <74948146+yashrajput25@users.noreply.github.com>
2024-05-18 16:02:26 +0530

    Dark Theme
    Added dark theme to the app

--- lib/main.dart
+++ lib/main.dart
@@ -6,13 +6,30 @@ import 'package:flutter/material.dart';
 var kColorScheme =
     ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 96, 59, 181));
 
+var kDarkColorScheme = ColorScheme.fromSeed(
+  brightness: Brightness.dark,
+  seedColor: Color.fromARGB(255, 5, 99, 125),
+);
+
 void main() {
   runApp(
       // ignore: prefer_const_constructors
 
       MaterialApp(
+          darkTheme: ThemeData.dark().copyWith(
+            colorScheme: kDarkColorScheme,
+            cardTheme: CardTheme().copyWith(
+              color: kDarkColorScheme.secondaryContainer,
+              margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+            ),
+            elevatedButtonTheme: ElevatedButtonThemeData(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: kDarkColorScheme.primaryContainer,
+                foregroundColor: kDarkColorScheme.onPrimaryContainer,
+              ),
+            ),
+          ),
           theme: ThemeData().copyWith(
-            useMaterial3: true,
             colorScheme: kColorScheme,
             /*color scheme on the basis of one colour automatically identifies other suitable combos of colours for the app*/
             appBarTheme: AppBarTheme().copyWith(
@@ -28,10 +45,13 @@ void main() {
               ),
             ),
             textTheme: ThemeData().textTheme.copyWith(
+                    /*in this whole scneario we are overriding some of the like titleLarge, flutter's default textthemes */
                     titleLarge: TextStyle(
-                  fontWeight: FontWeight.normal,
-                  fontSize: 14,
+                  fontWeight: FontWeight.bold,
+                  color: kColorScheme.onSecondaryContainer,
+                  fontSize: 16,
                 )),
           ),
+          themeMode: ThemeMode.dark,
           home: const Expenses()));
 }
